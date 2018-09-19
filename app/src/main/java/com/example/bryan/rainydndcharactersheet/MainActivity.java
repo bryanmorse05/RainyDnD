546e1fe94de27c7b283c8575ec495cf84c5e62ac
Bryan <bryanmorse05@gmail.com>
2018-09-18 19:38:09 -0700

    First commit with everything in it

---
+++ app/src/main/java/com/example/bryan/rainydndcharactersheet/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bryan.rainydndcharactersheet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //This is where the data for Rune Gemwall is going to go
+    }
+}
